69d02dc31baf7484c33ef03862a61743c1441758
karan singh <karanksrk2000@outlook.com>
2025-03-25 12:42:26 +0530

    first commit

---
+++ src/api/events/routing.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from .schema import EventSchema
+router = APIRouter()
+
+
+@router.get("/")
+def read_events():
+    return {
+        "results": [1, 2, 3],
+    }
+
+
+@router.get("/{event_id}")
+def get_events(event_id: int) -> EventSchema:
+    return {'id': event_id}
---
+++ src/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from api.events import router as event_router
+
+app = FastAPI()
+app.include_router(event_router, prefix="/api/events")
+
+
+@app.get("/")
+def get_root():
+    return {"message": "Hello World"}
+
+
+@app.get("/healthz")
+def read_api_health():
+    return {"status": "Ok"}
